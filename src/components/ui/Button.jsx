55717bd9b462ab9fd28198887fc8e38c79e071f3
Dmytro Anyduck <dmytro.anyduck@gmail.com>
2025-12-26 09:33:11 +0200

    feat: impove styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export function Button({ className, ...props }) {
   return (
     <button
-      className={`text-main-foreground bg-main border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none ${className}`}
+      className={`text-main-foreground bg-main border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none disabled:translate-x-0 disabled:translate-y-0 disabled:shadow-shadow disabled:opacity-50 ${className}`}
       {...props}
     />
   );
